f892d5b24314ec726520bd45121314066813d19e
ahmetkurucu <ahmetkurucu@hotmail.com.tr>
2026-01-06 19:37:04 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ const connectDB = require('../config/db');
 
 module.exports = async (req, res) => {
   try {
-    // DB gereken endpointler için bağlantıyı hazırla
-    await connectDB();
+    // sadece API istekleri zaten buraya düşüyor ama garanti olsun
+    if (req.url.startsWith('/api')) {
+      await connectDB();
+    }
     return app(req, res);
   } catch (e) {
     console.error('DB init error:', e.message);
